d451066eea9b23ea9f64ce34c5dccd00593405ae
Disc <78861687+Ekansh-Coder5@users.noreply.github.com>
2022-11-21 21:34:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+function div(){
+    window.location.href = "home1.html"
+}
+function divv(){
+    window.location.href = "home2.html"
+}
+function divvv(){
+    window.location.href = "home3.html"
+}
+trans();
+
+
+function light() {
+    document.getElementById('body2').style.backgroundColor ="white";
+    document.getElementById('h1').style.color ="black";
+    document.getElementById('h2').style.color ="black";
+    document.getElementById('h3').style.color ="black";
+    document.getElementById('h4').style.color ="black";
+    document.getElementById('main2').style.color ="black";
+    document.getElementById('g1').style.color ="black";
+    document.getElementById('g2').style.color ="black";
+    document.getElementById('head').style.color ="black";
+    document.getElementById('l1').style.color ="black";
+    document.getElementById('l2').style.color ="black";
+    document.getElementById('l3').style.color ="black";
+    document.getElementById('dark').style.color ="white";
+    document.getElementById('dark').style.backgroundColor ="black"; 
+    document.getElementById('light').style.color ="white";
+    document.getElementById('light').style.backgroundColor ="black"; 
+    document.getElementById('foot').style.backgroundColor ="black"; 
+    document.getElementById('h5').style.color ="white";
+    document.getElementById('a1').style.color ="white"; 
+    document.getElementById('a2').style.color ="white"; 
+
+}
+function dark() {
+    document.getElementById('body2').style.backgroundColor ="black";
+    document.getElementById('h1').style.color ="white";
+    document.getElementById('h2').style.color ="white";
+    document.getElementById('h3').style.color ="white";
+    document.getElementById('h4').style.color ="white";
+    document.getElementById('main2').style.color ="white";
+    document.getElementById('g1').style.color ="white";
+    document.getElementById('g2').style.color ="white";
+    document.getElementById('head').style.color ="white";
+    document.getElementById('l1').style.color ="white";
+    document.getElementById('l2').style.color ="white";
+    document.getElementById('l3').style.color ="white";
+    document.getElementById('light').style.color ="black";
+    document.getElementById('light').style.backgroundColor ="white";
+    document.getElementById('dark').style.color ="black";
+    document.getElementById('dark').style.backgroundColor ="white";
+    document.getElementById('foot').style.backgroundColor ="white"; 
+    document.getElementById('h5').style.color ="black";
+    document.getElementById('a1').style.color ="black"; 
+    document.getElementById('a2').style.color ="black"; 
+}
